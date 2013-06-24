4f2532f818a2ba9997d3610d32d7e5602c597578
Michael Bond <codemonkey85@gmail.com>
2013-06-24 00:46:32 -0400

    Some work on supporting Gen III (limited)

--- pkmds_gba.h
+++ pkmds_gba.h
@@ -1,83 +1,173 @@
 #pragma once
 #include "stdafx.h"
-struct trainerinfostruct
+#include <vector>
+struct gbapokemongrowth
 {
-    byte trainername[8];
-    byte playergender;
-    uint16 trainerid;
-    uint16 secretid;
-    uint16 hoursplayed;
-    byte minutesplayed;
-    byte secondsplayed;
-    byte framesplayed;
-    uint32 gamecode;
-    uint32 securitycode;
+	uint16 species;
+	uint16 item;
+	uint32 exp;
+	byte ppup;
+	byte tameness;
+	uint16 unknown;
 };
-struct team_itemstruct
+struct gbapokemonattacks
 {
-    byte data[1472];
+	uint16 moves[4];
+	byte pp[4];
 };
-struct unknown1struct
+struct gbapokemoneffort
 {
-    byte data[3968];
+	byte evs[6];
+	byte contest[6];
 };
-struct unknown2struct
+struct gbapokemonmisc
 {
-    byte data[3968];
+	byte pokerus;
+	byte locationcaught;
+	uint16 originsinfo;
+	uint32 ivs;
+	uint16 ribbons;
 };
-struct rivalinfostruct
+struct gbapokemonsubstruct
 {
-    byte data[3848];
+	gbapokemongrowth growth;
+	gbapokemonattacks attacks;
+	gbapokemoneffort effort;
+	gbapokemonmisc misc;
 };
+struct gbapokemon
+{
+	uint32 pid;
+	uint32 trainerid;
+	byte nickname[10];
+	uint16 language;
+	byte otname[7];
+	byte markings;
+	uint16 checksum;
+	uint16 unknown;
+	gbapokemonsubstruct encrypted;
+};
+struct gbasavefooter
+{
+	uint16 blockid;
+	uint16 checksum;
+	uint32 validation;
+	uint32 saveid;
+};
+struct gbasaveblock
+{
+	byte data[0xf80];
+	byte unknown[0x74];
+	gbasavefooter footer;
+};
+struct gbasavehalf
+{
+	gbasaveblock blocks[14];
+};
+struct gbasavefile
+{
+	gbasavehalf savefilea;
+	gbasavehalf savefileb;
+	byte unknown[0x4000];
+};
+void sortsaveblocks(gbasavehalf * savehalf);
+bool compareblocks(gbasaveblock *a, gbasaveblock *b);
+void sortblocks(std::vector<gbasaveblock*> * theblocks);
+void decryptgbapkm(gbapokemon * pkm);
+void shufflegbapkm(gbapokemon * pkm, bool un);
+void sortsavefile(gbasavefile * sav);
+void read(const char* file_name, gbasavefile *data);
+
+//struct trainerinfostruct
+//{
+//    //byte trainername[8];
+//    //byte playergender;
+//    //uint16 trainerid;
+//    //uint16 secretid;
+//    //uint16 hoursplayed;
+//    //byte minutesplayed;
+//    //byte secondsplayed;
+//    //byte framesplayed;
+//    //uint32 gamecode;
+//    //uint32 securitycode;
+//	byte data[3884];
+//};
+//struct team_itemstruct
+//{
+//    byte data[3968];
+//};
+//struct unknown1struct
+//{
+//    byte data[3968];
+//};
+//struct unknown2struct
+//{
+//    byte data[3968];
+//};
+//struct rivalinfostruct
+//{
+//    byte data[3848];
+//};
 //struct pcbuffstruct
 //{
 //    byte data[3968];
 //};
-enum sectionids : uint16
-{
-    trainerinfo,
-    team_items,
-    unknown1,
-    unknown2,
-    rivalinfo,
-    pcbuffa,
-    pcbuffb,
-    pcbuffc,
-    pcbuffd,
-    pcbuffe,
-    pcbufff,
-    pcbuffg,
-    pcbuffh,
-    pcbuffi
-};
-struct sectionfooter
-{
-    sectionids sectionid;
-    uint16 checksum;
-    uint32 saveindex;
-};
-struct datasection
-{
-    byte data[4080];
-    sectionfooter footer;
-};
-struct shuffledgamesavestruct
-{
-    datasection datasections[14];
-};
-struct gbaboxstruct
-{
-    byte data[30][80];
-};
-struct gamesavestruct
-{
-    trainerinfostruct trainerinfo_;
-    team_itemstruct team_items_;
-    unknown1struct unknown1_;
-    unknown2struct unknown2_;
-    rivalinfostruct rivalinfo_;
-    uint32 curbox;
-    gbaboxstruct boxes[14];
+//struct pcbuffstructi
+//{
+//    byte data[2000];
+//};
+//enum sectionids : uint16
+//{
+//    trainerinfo,
+//    team_items,
+//    unknown1,
+//    unknown2,
+//    rivalinfo,
+//    pcbuffa,
+//    pcbuffb,
+//    pcbuffc,
+//    pcbuffd,
+//    pcbuffe,
+//    pcbufff,
+//    pcbuffg,
+//    pcbuffh,
+//    pcbuffi
+//};
+//struct sectionfooter
+//{
+//    sectionids sectionid;
+//    uint16 checksum;
+//    uint32 saveindex;
+//};
+//struct datasection
+//{
+//    byte data[4080];
+//    sectionfooter footer;
+//	byte unknown[8];
+//};
+//struct shuffledgamesavesectionstruct
+//{
+//    datasection datasections[14];
+//};
+//struct shuffledgamesavestruct
+//{
+//    shuffledgamesavesectionstruct savea;
+//	shuffledgamesavesectionstruct saveb;
+//	byte unknown[16384];
+//};
+//struct gbaboxstruct
+//{
+//    byte data[30][80];
+//};
+//struct gamesavestruct
+//{
+//    trainerinfostruct trainerinfo_;
+//    team_itemstruct team_items_;
+//    unknown1struct unknown1_;
+//    unknown2struct unknown2_;
+//    rivalinfostruct rivalinfo_;
+//    //uint32 curbox;
+//    //gbaboxstruct boxes[14];
 //    pcbuffstruct pcbuffa_;
 //    pcbuffstruct pcbuffb_;
 //    pcbuffstruct pcbuffc_;
@@ -86,13 +176,13 @@ struct gamesavestruct
 //    pcbuffstruct pcbufff_;
 //    pcbuffstruct pcbuffg_;
 //    pcbuffstruct pcbuffh_;
-//    pcbuffstruct pcbuffi_;
-};
-struct gba_sav
-{
-public:
-    gamesavestruct savea;
-    gamesavestruct saveb;
-//private:
-    byte unused[16384];
-};
+//    pcbuffstructi pcbuffi_;
+//};
+//struct gba_sav
+//{
+//public:
+//    gamesavestruct savea;
+//    gamesavestruct saveb;
+////private:
+//    byte unused[16384];
+//};
