80184808fc5a55ce47594e8b825b8b76256a0ea0
Michael Bond <codemonkey85@gmail.com>
2013-08-05 08:28:06 -0400

    Started adding move category lookup.

--- QT Project/qt_sqlite.cpp
+++ QT Project/qt_sqlite.cpp
@@ -370,3 +370,32 @@ QPixmap getballpic(const Balls::balls ball)
     }
     return pixmap;
 }
+QPixmap getmovecatimage(const Moves::moves themove)
+{
+    QPixmap pixmap;
+    std::ostringstream o;
+    o << "select image from move_categories where (identifier = \"" << "" << ")";
+    const void * blob;
+    size_t thesize = 0;
+    char cmd[BUFF_SIZE];
+    strcpy_s(cmd,o.str().c_str());
+    if(sqlite3_prepare_v2(imgdatabase,cmd,-1,&imgstatement,0) == SQLITE_OK)
+    {
+        int cols = sqlite3_column_count(imgstatement);
+        int result = 0;
+        result = sqlite3_step(imgstatement);
+        if(result == SQLITE_ROW)
+        {
+            for(int col = 0; col < cols; col++)
+            {
+                blob = sqlite3_column_blob(imgstatement,col);
+                thesize = sqlite3_column_bytes(imgstatement,col);
+                QByteArray array;
+                array = QByteArray::fromRawData((const char*)blob,thesize);
+                pixmap.loadFromData(array);
+            }
+        }
+        sqlite3_finalize(imgstatement);
+    }
+    return pixmap;
+}
